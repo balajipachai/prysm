fd39c9aecd3c71a4b8c89c725c08f64a476b6688
Kasey Kirkham <kasey@users.noreply.github.com>
2023-06-11 11:20:48 -0500

    fixup! testnet generate-genesis support for deneb

--- cmd/prysmctl/testnet/generate_genesis.go
+++ cmd/prysmctl/testnet/generate_genesis.go
@@ -272,6 +272,7 @@ func generateGenesis(ctx context.Context) (state.BeaconState, error) {
 		if v > version.Altair {
 			// set ttd to zero so EL goes post-merge immediately
 			gen.Config.TerminalTotalDifficulty = big.NewInt(0)
+			gen.Config.TerminalTotalDifficultyPassed = true
 		}
 	} else {
 		gen = interop.GethTestnetGenesis(f.GenesisTime, params.BeaconConfig())
